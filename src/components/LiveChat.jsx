1897075f00b67c00f5ef5feea3eed95ff79f213a
Jayasuriya-R <suryaabi3801@gmail.com>
2025-05-10 14:22:04 +0530

    live chat scrolling

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -1,35 +1,41 @@
-import React, { useEffect } from 'react'
-import ChatMessage from './ChatMessage'
-import { Youtube_API_Key } from '../utils/constants'
-import { useDispatch, useSelector } from 'react-redux'
-import { addMessages } from '../utils/chatSlice'
+import React, { useEffect } from "react";
+import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessages } from "../utils/chatSlice";
 
 const LiveChat = () => {
+  const dispatch = useDispatch();
+  const messages = useSelector((store) => store.chatData.messages);
 
-    const dispatch = useDispatch();
-    const message = useSelector(store=> store.chatData.messages)
-    
-useEffect(()=>{
-const i = setInterval(()=>{
-//    api polling
-    const messageAPI= async () =>{
-        const data = await fetch("https://mocki.io/v1/552c8751-566e-4267-ae90-1f607cb2b71a");
-        const json = await data.json();
-        // console.log(json)
-        dispatch(addMessages(json))
-      }
-messageAPI()
-},2000)
+  useEffect(() => {
+    const i = setInterval(() => {
+      const messageAPI = async () => {
+        const data = await fetch(
+          "https://mocki.io/v1/552c8751-566e-4267-ae90-1f607cb2b71a"
+        );
+        const json = await data.json(); // ✅ Don't use `const` here
+        // dispatch(addMessages(json))
 
+        console.log("api polling")
+        json.forEach(msg => dispatch(addMessages(msg)));
+      };
+      // Stop after all messages are added
 
-return ()=> clearInterval(i)
-},[])
+      messageAPI();
+    }, 500);
+
+    return () => {
+      clearInterval(i);
+    };
+  }, []);
 
   return (
-    <div className='w-10/12 h-[500px] mx-6  p-2 border border-black rounded-lg bg-gray-100 overflow-y-scroll'>
-        { message.map((x,index)=><ChatMessage key={index} name={x.name} message={x.message}/>)}
+    <div className="w-10/12 h-[500px] mx-6 p-2 border border-black rounded-lg bg-gray-100 overflow-y-scroll flex flex-col-reverse">
+      {messages.map((x, index) => (
+        <ChatMessage key={index} name={x.name} message={x.message} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default LiveChat
+export default LiveChat;
--- src/utils/chatSlice.js
+++ src/utils/chatSlice.js
@@ -1,18 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { liveChatCount } from "./constants";
 
-const chatSlice = createSlice(
-    {
-        name:"chatData",
-        initialState:{
-            messages:[],
-        },
-        reducers:{
-            addMessages:(state,action) =>{
-                state.messages.push (...action.payload);
-            }
-        }
-    }
-)
+const chatSlice = createSlice({
+  name: "chatData",
+  initialState: {
+    messages: [],
+  },
+  reducers: {
+    addMessages: (state, action) => {
+      if (state.messages.length >= liveChatCount) {
+        state.messages.pop(); // removes last (oldest) message
+      }
+      state.messages.unshift(action.payload);
+    },
+  },
+});
 
-export const {addMessages} = chatSlice.actions;
-export default chatSlice.reducer
+export const { addMessages } = chatSlice.actions;
+export default chatSlice.reducer;
